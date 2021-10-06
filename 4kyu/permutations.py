eaebbad8d58d826d27e59b487576e3bd718c352c
RakhimbaevAD <61554698+RakhimbaevAD@users.noreply.github.com>
2021-10-06 20:09:18 +0300

    Delete 4kyu directory

--- 4kyu/permutations.py
+++
@@ -1,8 +0,0 @@
-import itertools
-
-
-def permutations(string):
-    result = list()
-    for i in itertools.permutations(string,len(string)):
-        result.append(''.join(i))
-    return set(result)
